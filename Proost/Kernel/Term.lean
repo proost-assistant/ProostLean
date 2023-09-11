09c817e8bc0276e86a7f8dedc4b5302cf26271bb
arthur-adjedj <arthur.adjedj@gmail.com>
2023-09-11 14:21:42 +0200

    feat : remove `Ann`, add `fvar`

--- Proost/Kernel/Term.lean
+++ Proost/Kernel/Term.lean
@@ -8,13 +8,13 @@ open Std
 abbrev Name := String
 
 inductive Term : Type :=
-  | var   : Nat → Term
+  | fvar   : Name → Term
+  | bvar   : Nat → Term
   | sort  : Level → Term
   | app   : Term → Term → Term
   | abs   : Option Term → Term → Term
   | prod  : Term → Term → Term
   | const : Name → Array Level →  Term
-  | ann   : Term → Term → Term
 deriving Repr, Inhabited, BEq
 
 abbrev VarContext := Array $ Option Term
@@ -34,60 +34,103 @@ def mkAppRange (f : Term) (i j : Nat) (args : Array Term) : Term :=
 
 namespace Term
 
+
+partial def abstract (n : Name): Term → Term :=
+  go 0
+where
+  go outer := fun
+    | fvar fv =>
+        if fv = n then
+          bvar outer
+        else 
+          fvar fv
+    | abs dom codom =>
+        abs (dom.map $ go outer) (go (outer+1) codom)
+    | prod dom codom =>
+        prod (go outer dom) (go (outer+1) codom)
+    | app f x => app (go outer f) (go outer x)
+    | t@(bvar ..) 
+    | t@(const ..)
+    | t@(sort ..) => t
+
+partial def instantiate (image : Term) : Term → Term :=
+  go 0
+where
+  go outer := fun
+    | bvar b =>
+      if b = outer then 
+        image
+      else 
+        bvar b
+    | abs dom codom => 
+        abs (dom.map $ go outer) (go (outer+1) codom)
+    | prod dom codom => 
+        prod (go outer dom) (go (outer+1) codom)
+    | app f x => app (go outer f) (go outer x)
+    | t@(fvar ..)
+    | t@(const ..) 
+    | t@(sort ..) => t
+
+
+@[inline]
+def substitute (image : Term) (name : Name) : Term → Term :=
+  Term.instantiate image ∘ Term.abstract name
+
+
+
 def getAppFn : Term →  Term
-  | .app f _ => f.getAppFn
+  | app f _ => f.getAppFn
   | t => t
 
 def getAppArgs : Term → Array Term
-  | .app f arg => f.getAppArgs.push arg
+  | app f arg => f.getAppArgs.push arg
   | _ => #[]
 
 def getAppFnArgs : Term →  Term × Array Term
-  | .app f arg =>
+  | app f arg =>
     let ⟨f,args⟩ := f.getAppFnArgs
     ⟨f,args.push arg⟩
   | t => ⟨t,#[]⟩
 
 def n_of_univ : Term → Nat 
-  | .var _ => 0
-  | .abs t₁ t₂ => 
+  | bvar _ | fvar _ => 0
+  | abs t₁ t₂ => 
     let t₁_univ := match t₁ with | some t => t.n_of_univ | none => 0
     max t₁_univ t₂.n_of_univ
-  | .app t₁ t₂ 
-  | .ann t₁ t₂
-  | .prod t₁ t₂ => max t₁.n_of_univ t₂.n_of_univ
-  | .const _ arr => arr.foldl (fun acc l => max acc l.n_of_univ) 0
-  | .sort l => l.n_of_univ
+  | app t₁ t₂ 
+  | prod t₁ t₂ => max t₁.n_of_univ t₂.n_of_univ
+  | const _ arr => arr.foldl (fun acc l => max acc l.n_of_univ) 0
+  | sort l => l.n_of_univ
 
 def prop : Term := .sort 0
 def type (l : Level) : Term := .sort l.succ
 
 def toString : Term → String
-    | .var i => s!"{i}"
-    | .sort l => s!"Sort {l}"
-    | .app t1 t2 => s!"({t1.toString}) ({t2.toString})"
-    | .abs (some t1) t2 => s!"λ {t1.toString} => {t2.toString}"
-    | .abs _ t2 => s!"λ _ => {t2.toString}"
-    | .prod t1 t2  => s!"Π ({t1.toString}). {t2.toString}"
-    | .const s #[] => s
-    | .const s arr => s ++ Array.toString₂ ".{" "," "}" arr
-    | .ann t ty => s!"({t.toString} : {ty.toString})"
+  | fvar n => n
+  | bvar i => s!"{i}"
+  | sort l => s!"Sort {l}"
+  | app t1 t2 => s!"({t1.toString}) ({t2.toString})"
+  | abs (some t1) t2 => s!"λ {t1.toString} => {t2.toString}"
+  | abs _ t2 => s!"λ _ => {t2.toString}"
+  | prod t1 t2  => s!"Π ({t1.toString}). {t2.toString}"
+  | const s #[] => s
+  | const s arr => s ++ Array.toString₂ ".{" "," "}" arr
 
 instance : ToString Term := ⟨Term.toString⟩
 
 def substitute_univ (lvl : Array Level) : Term → Term
   | sort l => sort $ l.substitute lvl
-  | var n => var n
+  | bvar n => bvar n
+  | fvar fv => fvar fv
   | app t₁ t₂ => app (t₁.substitute_univ lvl) (t₂.substitute_univ lvl)
   | abs ty body => abs (ty.attach |>.map (λ ⟨e,_⟩ => substitute_univ lvl e)) (body.substitute_univ lvl)
   | prod a b => prod (a.substitute_univ lvl) (b.substitute_univ lvl)
-  | ann t ty => ann (t.substitute_univ lvl) (ty.substitute_univ lvl) 
   | const s arr => const s $ arr.map (Level.substitute · lvl)
 
 def shift (offset depth : Nat) : Term → Term
-  | var n => 
+  | bvar n => 
     let n := if n >= depth then n+offset else n
-    var n
+    bvar n
   | app t₁ t₂ => app (t₁.shift offset depth) (t₂.shift offset depth)
   | abs ty body =>
     let ty   := ty.attach.map (λ ⟨e,_⟩ => shift offset depth e)
@@ -97,15 +140,14 @@ def shift (offset depth : Nat) : Term → Term
     let ty := ty.shift offset depth
     let body := body.shift offset depth.succ
     prod ty body
-  | ann t ty => ann (t.shift offset depth) (ty.shift offset depth)
   | const s l => const s l
   | sort l => sort l
 
 def substitute (self sub : Term) (depth : Nat) : Term := match self with
-  | var n => match compare n depth with
+  | bvar n => match compare n depth with
       | .eq => sub.shift depth.pred 1
-      | .gt => var (n-1)
-      | .lt => var n
+      | .gt => bvar (n-1)
+      | .lt => bvar n
   | app t₁ t₂ => app (t₁.substitute sub depth) (t₂.substitute sub depth)
   | abs ty body => 
     let ty := ty.attach.map (λ ⟨e,_⟩ => substitute e sub depth)
@@ -115,7 +157,6 @@ def substitute (self sub : Term) (depth : Nat) : Term := match self with
     let ty := ty.substitute sub depth
     let body := body.substitute sub depth.succ
     prod ty body
-  | ann t ty => ann (t.substitute sub depth) (ty.substitute sub depth)
   | const s l => const s l
   | sort l => sort l
 
